a3a128610efc669f514fde5a455e5f31c602a907
keyaira91 <keyaira22@hotmail.com>
2021-08-23 23:02:16 -0400

    put game into one function

--- Homework/homework_5.py
+++ Homework/homework_5.py
@@ -1,74 +1,73 @@
-import random
+def play_craps():
 
-def first_roll():
-    one = random.randint(1,6)
-    two = random.randint(1,6)
-    dice = one + two
-    print("\nFirst Roll:")
-    print(f"You rolled a {dice}. Die faces were {one} and {two}")
-    point.append(dice)
-    if dice == 7 or dice == 11:
-        print("You Win!")
-    elif dice == 2 or dice == 3 or dice == 12:
-        print("You Lose!")
+    import random
 
+    def first_roll():
+        one = random.randint(1,6)
+        two = random.randint(1,6)
+        dice = one + two
+        print("\nFirst Roll:")
+        print(f"You rolled a {dice}. Die faces were {one} and {two}")
+        point.append(dice)
+        if dice == 7 or dice == 11:
+            print("You Win!")
+        elif dice == 2 or dice == 3 or dice == 12:
+            print("You Lose!")
 
-def next_roll():
-    one = random.randint(1,6)
-    two = random.randint(1,6)
-    dice = one + two
-    print(f"You rolled a {dice}. Die faces were {one} and {two}")
-    if dice == 7:
-        print("You Lose!")
-        play_again = input("\nYou lost! But you can always play again: (y/n)")
-        if play_again == 'n':
-            exit()
-        else:
-            point.pop()
-            first_roll()
-    elif dice != point[0]:
-         print(input("Press enter to roll again"))
-         next_roll()
-    else:
-        print("You Win!")
-        play_again2 = input("\nYou won! Do you want to play again: (y/n)")
-        if play_again2 == 'y':
-            point.pop()
-            first_roll()
-        else:
-            exit()
-
-#-------------------------------------------------------------------------------
-
-point = []
-dice = 0
-
-print("\nLet's Play Craps!")
-print(input("Press enter to roll the dice!"))
-
-first_roll()
-
-while dice != point[0]:
-    if point[0] == 2 or point[0] == 3 or point[0] == 12:
-        play_again = input("\nYou lost! But you can always play again: (y/n)")
-        if play_again == 'n':
+    def next_roll():
+        one = random.randint(1,6)
+        two = random.randint(1,6)
+        dice = one + two
+        print(f"You rolled a {dice}. Die faces were {one} and {two}")
+        if dice == 7:
+            print("You Lose!")
+            play_again = input("\nYou lost! But you can always play again: (y/n)")
+            if play_again == 'n':
                 exit()
-        else:
+            else:
                 point.pop()
                 first_roll()
-    elif point[0] == 7 or point[0] == 11:
-        play_again2 = input("\nYou won! Do you want to play again: (y/n)")
-        if play_again2 == 'n':
-            exit()
+        elif dice != point[0]:
+            print(input("Press enter to roll again"))
+            next_roll()
         else:
-            point.pop()
-            first_roll()
-    else:
-        print(f"\nOn the next turn, you'll need to roll a {point[0]} to win.")
-        print(input("Press enter for next roll"))
-        next_roll()
+            print("You Win!")
+            play_again2 = input("\nYou won! Do you want to play again: (y/n)")
+            if play_again2 == 'y':
+                point.pop()
+                first_roll()
+            else:
+                exit()
 
+    point = []
+    dice = 0
 
+    print("\nLet's Play Craps!")
+    print(input("Press enter to roll the dice!"))
+
+    first_roll()
+
+    while dice != point[0]:
+        if point[0] == 2 or point[0] == 3 or point[0] == 12:
+            play_again = input("\nYou lost! But you can always play again: (y/n)")
+            if play_again == 'n':
+                    exit()
+            else:
+                    point.pop()
+                    first_roll()
+        elif point[0] == 7 or point[0] == 11:
+            play_again2 = input("\nYou won! Do you want to play again: (y/n)")
+            if play_again2 == 'n':
+                exit()
+            else:
+                point.pop()
+                first_roll()
+        else:
+            print(f"\nOn the next turn, you'll need to roll a {point[0]} to win.")
+            print(input("Press enter for next roll"))
+            next_roll()
+
+play_craps()
 
 
 
