878341d595751c953ff8444243c76be6793f7cd7
keyaira91 <keyaira22@hotmail.com>
2021-09-17 19:14:16 -0400

    displaying level number

--- Homework/Alien Invasion/alien_invasion.py
+++ Homework/Alien Invasion/alien_invasion.py
@@ -63,6 +63,7 @@ class AlienInvasion:
             self.stats.reset_stats()
             self.stats.game_active = True
             self.sb.prep_score()
+            self.sb.prep_level()
 
             # Get rid of any remaining aliens anf bullets.
             self.aliens.empty()
@@ -128,6 +129,10 @@ class AlienInvasion:
             self.bullets.empty()
             self._create_fleet()
             self.settings.increase_speed()
+
+            # Increase level
+            self.stats.level += 1
+            self.sb.prep_level()
 
     def _ship_hit(self):
         """Respond to the ship being hit by an alien."""
