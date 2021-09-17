fb0ae1206f70ab36aaedc6d1b3c1dbec388cd2c7
keyaira91 <keyaira22@hotmail.com>
2021-09-17 15:37:41 -0400

    check if aliens hit the bottom of the screen

--- Homework/Alien Invasion/alien_invasion.py
+++ Homework/Alien Invasion/alien_invasion.py
@@ -120,6 +120,9 @@ class AlienInvasion:
         # Look for alien-ship collisions
         if pygame.sprite.spritecollideany(self.ship, self.aliens):
             self._ship_hit()
+
+        # look for aliens hitting the botton of the screen.
+        self._check_aliens_bottom()
             
 
     def _create_fleet(self):
@@ -164,6 +167,16 @@ class AlienInvasion:
             alien.rect.y += self.settings.fleet_drop_speed
         self.settings.fleet_direction *= -1
 
+
+    def _check_aliens_bottom(self):
+        """Check if any aliens have reached the bottom of the screen."""
+        screen_rect = self.screen.get_rect()
+        for alien in self.aliens.sprites():
+            if alien.rect.bottom >= screen_rect.bottom:
+                # Treat this the same as if the ship got hit.
+                self._ship_hit()
+                break
+
     def _update_screen(self):
         """Update images on the screen, and flip to the new screen."""
         self.screen.fill(self.settings.bg_color)
@@ -174,6 +187,7 @@ class AlienInvasion:
 
         # Make the most recently drawn screen visible.
         pygame.display.flip()
+
 
 if __name__ == '__main__':
     # Make a game instance, run the game.
