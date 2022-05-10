5e77c42fd9aa6935f72f165a8f2e45d7e744d466
keyaira91 <keyaira22@hotmail.com>
2022-05-10 02:17:11 -0400

    updated ui

--- weatherapp/app/views.py
+++ weatherapp/app/views.py
@@ -11,23 +11,62 @@ def index(request):
 
     if request.method == "POST":
         city = request.POST['city']
+        url = (f'https://api.openweathermap.org/geo/1.0/direct?q={city}&appid=' + settings.WEATHER_API_KEY)
+        r = requests.get(url)
+        data = r.json()
 
-        url = (f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid=' + settings.WEATHER_API_KEY)
+        lat = data[0]['lat']
+        lon = data[0]['lon']
+
+        url = (f'https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=' + settings.WEATHER_API_KEY)
         r = requests.get(url)
         city_data = r.json()
-        time_stamp  = city_data['dt']
-         
+
+        time_stamp  = city_data['list'][0]['dt']
+        next_day = city_data['list'][8]['dt']
+        next_day2 = city_data['list'][16]['dt']
+        next_day3 = city_data['list'][24]['dt']
+        next_day4 = city_data['list'][32]['dt']
+ 
+
 
         data = {
-            "name": str(city_data['name']),
-            "country_code": str(city_data['sys']['country']),
-            "date": str(datetime.fromtimestamp(time_stamp).strftime("%w %b")),
-            "temp": str(math.floor(city_data['main']['temp'] - 273.15) * 9/5 + 32) + '°F',
-            "feels_like": str(math.floor(city_data['main']['feels_like'] - 273.15) * 9/5 + 32) + '°F',
-            "temp_min": str(math.floor(city_data['main']['temp_min'] - 273.15) * 9/5 + 32) + '°F',
-            "main": str(city_data['weather'][0]['main']),
-            "description": str(city_data['weather'][0]['description'].title()),
-            "icon": str(city_data['weather'][0]['icon']),
+            "name": str(data[0]['name']),
+            "state": str(data[0]['state']),
+            "country_code": str(data[0]['country']),
+            "date": str(datetime.fromtimestamp(time_stamp).strftime("%A, %b %d")),
+            "temp": str(int(math.floor(city_data['list'][0]['main']['temp'] - 273.15) * 9/5 + 32)) + '°F',
+            "lows": str(int(math.floor(city_data['list'][0]['main']['temp_min'] - 273.15) * 9/5 + 32)) + '°F',
+            "highs": str(int(math.floor(city_data['list'][0]['main']['temp_max'] - 273.15) * 9/5 + 32)) + '°F',
+            "feels_like": str(int(math.floor(city_data['list'][0]['main']['feels_like'] - 273.15) * 9/5 + 32)) + '°F',
+            "main": str(city_data['list'][0]['weather'][0]['main']),
+            "description": str(city_data['list'][0]['weather'][0]['description'].title()),
+            "icon": str(city_data['list'][0]['weather'][0]['icon']),
+
+            # This Week's Weather
+            # Day One
+            "next_day": str(datetime.fromtimestamp(next_day).strftime("%a \t")),
+            "next_temp": str(int(math.floor(city_data['list'][8]['main']['temp'] - 273.15) * 9/5 + 32)) + '°',
+            "next_main": str(city_data['list'][8]['weather'][0]['main']),
+            "next_icon": str(city_data['list'][8]['weather'][0]['icon']),
+            
+            # Day Two
+            "next_day2": str(datetime.fromtimestamp(next_day2).strftime("%a \t")),
+            "next_temp2": str(int(math.floor(city_data['list'][16]['main']['temp'] - 273.15) * 9/5 + 32)) + '°',
+            "next_main2": str(city_data['list'][16]['weather'][0]['main']),
+            "next_icon2": str(city_data['list'][16]['weather'][0]['icon']),
+
+            # Day Three
+            "next_day3": str(datetime.fromtimestamp(next_day3).strftime("%a \t")),
+            "next_temp3": str(int(math.floor(city_data['list'][24]['main']['temp'] - 273.15) * 9/5 + 32)) + '°',
+            "next_main3": str(city_data['list'][24]['weather'][0]['main']),
+            "next_icon3": str(city_data['list'][24]['weather'][0]['icon']),
+
+            # Day Four
+            "next_day4": str(datetime.fromtimestamp(next_day4).strftime("%a \t")),
+            "next_temp4": str(int(math.floor(city_data['list'][32]['main']['temp'] - 273.15) * 9/5 + 32)) + '°',
+            "next_main4": str(city_data['list'][32]['weather'][0]['main']),
+            "next_icon4": str(city_data['list'][32]['weather'][0]['icon']),
         }
     else:
         data = {}
